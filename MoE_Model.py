b58c0498d604fe09cd4b7400f3d4fa661ba20e97
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-10-20 09:39:26 +0200

    General template for modules created

--- MoE_Model.py
+++ MoE_Model.py
@@ -38,19 +38,45 @@ class Gate(nn.Module):
 
     def forward(self, x):
         x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        logits = self.gating(x)
         return logits
 
 
 
 class Expert(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.gating = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
 
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 
 
 
 class Output(nn.Module):
-    
+    def __init__(self):
+        super().__init__()
+        self.gating = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 
 
